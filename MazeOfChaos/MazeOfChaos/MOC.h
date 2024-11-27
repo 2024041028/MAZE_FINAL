143736bcb525da0de0964654d36e0f78905df6c4
yunhanuk <hanugyun@gmail.com>
2024-11-28 00:07:33 +0900

    MOC.H 추가

---
+++ MazeOfChaos/MazeOfChaos/MOC.h
@@ -0,0 +1,27 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+#include <conio.h>
+#include <time.h>
+
+// ******* cursor_function.c에 존재 ******* //
+void MoveConsole();
+void RemoveCursor();
+void SetColor();
+
+// *************************************** //
+
+// ******* screen_function.c에 존재 ******* //
+void CreateOutFrame();
+void ScreenReset();
+void FinishGame();
+// *************************************** //
+
+// ******* screen_YHU.c에 존재 *********** //
+void ExplainGame();
+void StartScreen();
+// ************************************** //
+
