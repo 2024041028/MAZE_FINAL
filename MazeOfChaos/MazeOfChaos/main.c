a0afdbdeece8cd986077377b747a35888c9a0369
yunhanuk <hanugyun@gmail.com>
2024-12-01 00:14:10 +0900

    터미널의 이름을 변경함

--- MazeOfChaos/MazeOfChaos/main.c
+++ MazeOfChaos/MazeOfChaos/main.c
@@ -1,6 +1,13 @@
 #include "MOC.h"
 
+char user_name[20];
+int user_info[12];
+int user_record[6];
+
 int main() {
+	system("title Maze of Chaos");
 	RemoveCursor();
 	StartScreen();
+	MenuScreen();
+	FinishGame();
 }
