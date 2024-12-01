dbcdd4bec5d6ff9adeab16acbf2bc04a46667b33
2024041028 <dbsqja3838@gmail.com>
2024-12-01 21:40:31 +0900

    main.c 함수 수정

--- MazeOfChaos/MazeOfChaos/main.c
+++ MazeOfChaos/MazeOfChaos/main.c
@@ -4,10 +4,10 @@ char user_name[20];
 int user_info[12];
 int user_record[6];
 
-int main() {
-	system("title Maze of Chaos");
-	RemoveCursor();
-	StartScreen();
-	MenuScreen();
-	FinishGame();
-}
+//int main() {
+	//system("title Maze of Chaos");
+	//RemoveCursor();
+	//StartScreen();
+	//MenuScreen();
+	//FinishGame();
+//}
