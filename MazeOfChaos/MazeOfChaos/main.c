848f67ccf819116863a53abbc0ddaeb0c42c937d
2024041028 <dbsqja3838@gmail.com>
2024-12-07 11:26:27 +0900

    main 함수 수정

--- MazeOfChaos/MazeOfChaos/main.c
+++ MazeOfChaos/MazeOfChaos/main.c
@@ -4,17 +4,12 @@ int main() {
 	system("title Maze of Chaos");
 	RemoveCursor();
 	CreateOutFrame();
-<<<<<<< HEAD
 	//StartScreen();
 	//MenuScreen();
 	//FinishGame();
-	PlayMemoryGame();
-=======
-	StartScreen();
-    MenuScreen();
-	FinishGame();
-	//PlayHangman();
-	//PlayMathGame();
->>>>>>> 3f329b8e4d18094772ef44bc25412b2ee7e190df
+    //MenuScreen();
+	//FinishGame();
+	PlayReactionGame();
+	
 }
 
