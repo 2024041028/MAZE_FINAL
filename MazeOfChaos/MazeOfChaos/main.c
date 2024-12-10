e5f7a048ef78c0d89542ab58a45d6b10e9003a20
kbg1227 <leo20051227@gmail.com>
2024-12-10 18:14:46 +0900

    병합

--- MazeOfChaos/MazeOfChaos/main.c
+++ MazeOfChaos/MazeOfChaos/main.c
@@ -5,15 +5,7 @@ int main() {
 	system("title Maze of Chaos");
 	RemoveCursor();
 	CreateOutFrame();
-<<<<<<< HEAD
-	//PlayTriviaQuizGame(1);
 	StartScreen();
 	MenuScreen();
 	FinishGame();
-	
-=======
-	StartScreen();
-	MenuScreen();
-	FinishGame();
->>>>>>> c77b26377df7507c13f0236dabd6c28cfdcaf36f
 }
