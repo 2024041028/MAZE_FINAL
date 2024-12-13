2ffa26ff310a28abf67c0ff85e98848211cb8748
yunhanuk <hanugyun@gmail.com>
2024-12-13 19:04:07 +0900

    쓰레기 문자 제거 함수 보완

--- MazeOfChaos/MazeOfChaos/manage_newline.c
+++ MazeOfChaos/MazeOfChaos/manage_newline.c
@@ -3,18 +3,18 @@
 void RemoveGarbageChar() {
     char newline[100];
     char ch;
-
     while (_kbhit()) { // 입력 버퍼에 남아 있는 키 처리
         ch = _getch();
-        if (ch == '\r') {
+        if(ch == -32 || ch == 0) { // 방향키 및 특수키 감지
+            ch = _getch(); // 두 번째 호출로 방향키의 실제 키 코드 가져오기
+            continue;      // 방향키 무시
+        }
+        else if (ch == '\r') { // 엔터키 무시
             continue;
         }
-        else { // 방향키, 쓸모없는 키
+        else { // 일반 입력
             while (_kbhit()) {
-                ch = _getch();
-                if (_kbhit() != 0) {
-                    break;
-                }
+                ch = _getch(); // 입력 버퍼 비우기
             }
         }
     }
